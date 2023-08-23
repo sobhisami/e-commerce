2cd5199cfa37ed2577436d9bda0f90f6ec1374d9
sobhisami <sobhisami073@gmail.com>
2023-08-23 12:18:33 +0300

    structure app and color

---
+++ src/app/theme/index.js
@@ -0,0 +1,70 @@
+export const mainTheme = {
+    colors: {
+        white:"#FFF",
+        dark: "#1C1C1C",
+        blue: "#0D6EFD",
+        blue100: "#E3F0FF",
+        green: "#00B517",
+        green100: "#C4FFCB",
+        orange: "#FF9017",
+        orange100: "#FFE6BF",
+        red: "#FA3434",
+        red100: "#FFE3E3",
+        gray: {
+            gray100: "#F7FAFC",
+            gray200: "#EFF2F4",
+            gray300: "#E3E8EE",
+            gray400: "#BBBCBE",
+            gray500: "#8B96A5",
+            gray600: "#505050",
+            gray800: "#606060",
+        }
+    },
+    fonts: {
+        h1: {
+            size: "font-weight: 600; font-size: 4.5rem;", // 72px
+            height: "line-height: 4.875rem;",
+            spacing: "letter-spacing: -1.6px;"
+        },
+        h2: {
+            size: "font-weight: 700; font-size: 3rem;", // 48px
+            height: "line-height: 3.625rem;",
+            spacing: "letter-spacing: -1px;"
+        },
+        h3: {
+            size: "font-weight: 600; font-size: 2rem;", // 32px
+            height: "line-height: 2.43rem;",
+            spacing: "letter-spacing: -0.2px;"
+        },
+        h4: {
+            size: "font-weight: 600; font-size: 1.5rem;", // 24px
+            height: "line-height: 2rem;",
+            spacing: "letter-spacing: -0.2px;"
+        },
+        h5: {
+            size: "font-weight: 600;font-size: 1.25rem;", // 20px
+            height: "line-height: 1.625rem;",
+            spacing: "letter-spacing: -0.2px;"
+        },
+        h6: {
+            size: "font-weight: 600;font-size: 1rem;", // 16px
+            height: "line-height: 1.375rem;",
+            spacing: "letter-spacing: -0.1px;"
+        },
+        body1: {
+            size: "font-weight: 400;font-size: 1rem;", // 16px
+            height: "line-height: 1.5rem;",
+            spacing: "letter-spacing: -0.2px;"
+        },
+        body2: {
+            size: "font-weight: 400;font-size: 0.875rem;", // 14px
+            height: "line-height: 1rem;",
+            spacing: "letter-spacing: 0px;"
+        },
+        body3: {
+            size: "font-weight: 400;font-size: 0.75rem;", // 12px
+            height: "line-height: 1rem;",
+            spacing: "letter-spacing: 0px;"
+        },
+    }
+}
